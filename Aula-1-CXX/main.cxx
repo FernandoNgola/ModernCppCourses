998bd69e844f23ca82b2d4736cc87c06edd57544
FernandoNgola <48277773+FernandoNgola@users.noreply.github.com>
2020-11-19 18:52:22 +0100

    Update main.cxx

--- Aula-1-CXX/main.cxx
+++ Aula-1-CXX/main.cxx
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <algorithm>
+#include <ranges>
+#include <vector>
 
 auto main() -> int
-{	
+{	std::vector<int> my_v{1,-5, 6, 2, -8};
+	std::ranges::sort(my_v);
 	std::cout << "Hello World\n"
 		     "This is \"The Modern C++\""<<std::endl;
+	std::ranges::for_each(my_v, [](int i){std::cout<<i<<", ";});
 }
